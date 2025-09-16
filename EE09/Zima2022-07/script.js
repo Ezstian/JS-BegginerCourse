d5865e69243c11d495db26dc83472b2e97496b75
Ezstian <krystiankc243@gmail.com>
2025-09-16 17:28:58 +0200

    koniecez

--- EE09/Zima2022-07/script.js
+++ EE09/Zima2022-07/script.js
@@ -1,8 +1,11 @@
-// function icon(){
-//     let iconSrc = document.getElementById('likeicon').src;
-//     if(iconSrc=="icon-off.png")
-//         iconSrc = "icon-on.png"
-//         else
-//             iconSrc = "icon-off.png"
-        
-//     }
+function icon(){
+    let iconSrc = document.getElementById('likeIcon').getAttribute("src");
+    console.log(iconSrc);
+    if(iconSrc=="icon-off.png")
+            document.getElementById('likeIcon').setAttribute("src","icon-on.png")
+        else
+            document.getElementById('likeIcon').setAttribute("src","icon-off.png")
+    }
+    function largeImage(url){
+        document.getElementById("largeImage").setAttribute("src",url)
+    }
