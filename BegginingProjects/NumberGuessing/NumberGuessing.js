cc32bdd88505f8626e99399c66cc5de6a4fcc69c
Ezstian <krystiankc243@gmail.com>
2025-09-16 22:14:13 +0200

    Number guessing game

--- BegginingProjects/NumberGuessing/NumberGuessing.js
+++ BegginingProjects/NumberGuessing/NumberGuessing.js
@@ -1,5 +1,18 @@
-console.log("Welcome to the Number Guessing Game!");
+let RandomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(RandomNumber);
 
-let randomNumber = Math.floor(Math.random()*100);
+function Guess() {
+    let userGuess = Number(document.getElementById("guessInput").value);
 
-console.log(randomNumber);
+    if (userGuess < 1 || userGuess > 100) {
+        document.getElementById("result").innerHTML = "Please enter a number between 1 and 100";
+    } else if (userGuess === RandomNumber) {
+        document.getElementById("result").innerHTML = "Congratulations! You guessed the number!";
+    } else if (userGuess < RandomNumber) {
+        document.getElementById("result").innerHTML = "Too low! Try again.";
+    } else if (userGuess > RandomNumber) {
+        document.getElementById("result").innerHTML = "Too high! Try again.";
+    }
+}
+ 
+
