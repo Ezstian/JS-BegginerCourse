e780dc84a1268e556c75a5bce92a7304ac32ec67
Ezstian <krystiankc243@gmail.com>
2025-09-16 17:08:48 +0200

    serducho dziala

---
+++ EE09/Zima2022-07/script.js
@@ -0,0 +1,8 @@
+// function icon(){
+//     let iconSrc = document.getElementById('likeicon').src;
+//     if(iconSrc=="icon-off.png")
+//         iconSrc = "icon-on.png"
+//         else
+//             iconSrc = "icon-off.png"
+        
+//     }
